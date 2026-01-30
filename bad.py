28682cd00e1b0d221ce05a2fd0061e5b7fcd369e
vishalakshichakilam <vishalakshi.chakilam@nxtwave.co.in>
2026-01-30 15:42:23 +0530

    Update bad.py

--- bad.py
+++ bad.py
@@ -1,6 +1,13 @@
 import sqlite3
 
 def login(user):
+    # Connect to database
     conn = sqlite3.connect("users.db")
+
+    # ❌ Vulnerable SQL Injection (OWASP A03: Injection)
     query = "SELECT * FROM users WHERE name = '" + user + "'"
+
+    # Execute unsafe query
     conn.execute(query)
+
+    print("Login query executed!")
